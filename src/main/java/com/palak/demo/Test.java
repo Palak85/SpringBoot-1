8f5f18df3d924c19a9668db09da3b9335b95761d
Palak Kumari <149322374+Palak85@users.noreply.github.com>
2026-07-13 12:43:03 +0530

    IOC

--- src/main/java/com/palak/demo/Test.java
+++ src/main/java/com/palak/demo/Test.java
@@ -1,4 +1,20 @@
 package com.palak.demo;
 
+class P{
+    void m1(){
+
+    }
+
+    void m2(){
+
+    }
+}
+class c1 extends P{
+    void m3(){
+
+    }
+}
 public class Test {
+    c1 c = new c1();
+//    c1 a = new P(); // we cannot do this because we cannot store the object of parent class into the child reference
 }
