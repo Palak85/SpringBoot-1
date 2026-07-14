407b65ded73ce177498f98296582739cb7f44450
Palak Kumari <149322374+Palak85@users.noreply.github.com>
2026-07-14 15:32:05 +0530

    StudentServer

--- src/main/java/com/palak/demo/StudentServer/StudentServer.java
+++ src/main/java/com/palak/demo/StudentServer/StudentServer.java
@@ -5,21 +5,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController // to listen http incoming request
 public class StudentServer {
     // 1. Store the Student - post http method
-    @PostMapping("/create")
-    public String storeStudent(@RequestBody Student student) {
-        int id = student.getId();
-        String name = student.getName();
-        int age = student.getAge();
-        String department = student.getDepartment();
-
-        return "id: " +id +
-                ", name: " +name +
-                ", age: " +age +
-                ", department: " +department;
-    }
+//    @PostMapping("/create")
+//    public String storeStudent(@RequestBody Student student) {
+//        int id = student.getId();
+//        String name = student.getName();
+//        int age = student.getAge();
+//        String department = student.getDepartment();
+//
+//        return "id: " +id +
+//                ", name: " +name +
+//                ", age: " +age +
+//                ", department: " +department;
+//    }
 
 //        return """
 //                id : 1,
