cbab05874e418a168a884602e815f4b20b3b4d36
Palak Kumari <149322374+Palak85@users.noreply.github.com>
2026-07-15 11:31:51 +0530

    Data Jpa

--- src/main/java/com/palak/demo/StudentServer/StudentRepository.java
+++ src/main/java/com/palak/demo/StudentServer/StudentRepository.java
@@ -1,12 +1,17 @@
 package com.palak.demo.StudentServer;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class StudentRepository {
+//@Repository
 
-    public Student save(Student student){
-        System.out.println("Student Information saved");
-        return student;
-    }
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+    //JpaRepository :- is an interface and it have methods and we need to implements its methods which will be done by spring-data-jpa
+
+
+//    public Student save(Student student){
+//        System.out.println("Student Information saved");
+//        return student;
+//    }
 }
